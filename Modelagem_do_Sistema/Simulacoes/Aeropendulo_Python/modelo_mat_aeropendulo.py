5b6bd5f1c7d6d57ddd429426958fa760331035e9
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-02-10 14:59:29 -0300

    repositorio atualizado

--- Modelagem_do_Sistema/Simulacoes/Aeropendulo_Python/modelo_mat_aeropendulo.py
+++ Modelagem_do_Sistema/Simulacoes/Aeropendulo_Python/modelo_mat_aeropendulo.py
@@ -20,7 +20,8 @@ class ModeloAeropendulo:
         self.t_simu = 0
         self.ts = 0
         self.t = 0
-        self.x = 0
+        self.x1 = [[], []]
+        self.x_ = 0
 
     # Define o nome da função que modela o sistema;
     def modelo_aeropendulo(self, x, t):
@@ -36,27 +37,43 @@ class ModeloAeropendulo:
         self.t_simu = t_simu
         self.ts = ts
         self.t = ts*np.arange(0, t_simu+ts, ts)
-
         # Condições iniciais
-        x_0 = x_0
-
+        self.x_0 = x_0
         # Integração com método odeint() da biblioteca scipy.integrate
-        self.x = odeint(self.modelo_aeropendulo, x_0, self.t)
-        return self.x
+        self.x_ = odeint(self.modelo_aeropendulo, self.x_0, self.t)
+        return self.x_
+
+    def simulacao_dinamica(self, t_simu=100, ts=0.1, x_0=[0.1, -0.5]):
+        self.t_simu = t_simu
+        self.ts = ts
+        self.t = ts*np.arange(0, t_simu+ts, ts)
+        self.x = x_0
+        for j, i in enumerate(self.t):
+            dx = self.modelo_aeropendulo(self.x, self.t)
+            try:
+                dt = (self.t[j+1]-self.t[j])
+            except:
+                pass
+            self.x = self.x + dt * dx
+            self.x1[0].append(self.x[0])
+            self.x1[1].append(self.x[1])
 
     def plotar_graficos(self):
         plt.figure(figsize=(10, 7))
         plt.suptitle("Gráficos dos estados do Aeropêndulo")
 
         plt.subplot(211)
-        plt.plot(self.t, self.x[:, 0])
+        plt.plot(self.t, self.x_[:, 0], lw=3.5)
+        plt.plot(self.t, self.x1[0])
 
         plt.subplot(212)
-        plt.plot(self.t, self.x[:, 1])
+        plt.plot(self.t, self.x_[:, 1], lw=3.5)
+        plt.plot(self.t, self.x1[1])
         plt.show()
 
 
 if __name__ == "__main__":
     aeropendulo_1 = ModeloAeropendulo()
-    aeropendulo_1.simular()
+    aeropendulo_1.simular(t_simu=1e3, ts=1e-2)
+    aeropendulo_1.simulacao_dinamica(t_simu=1e3, ts=1e-2)
     aeropendulo_1.plotar_graficos()
