84f6dbedbe33333eae3507dd18ed266906fd40d8
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-04-18 18:40:07 -0300

    repositorio atualizado

--- Identificacao_de_Sistemas/CODIGOS_MICROCONTROLADORES/motor_gerador_ino/teste_aeropendulo_malha_aberta/teste_aeropendulo_malha_aberta.ino
+++ Identificacao_de_Sistemas/CODIGOS_MICROCONTROLADORES/motor_gerador_ino/teste_aeropendulo_malha_aberta/teste_aeropendulo_malha_aberta.ino
@@ -1,25 +1,28 @@
 int pinoAD = A0;
-int pinoPWM = 9;
+int pinoPWM = 10;
 
 int valorAD = 0;
-int valor_pwm = 0;
+int valor_pwm = 100;
 
-int pinoSentido1 = 7;
-int pinoSentido2 = 8;
+int pinoSentido1 = 8;
+int pinoSentido2 = 9;
+int LED = 13;
 
 void setup() {
   pinMode(pinoSentido1, OUTPUT);
-  pinMode(pinoSentido2, OUTPUT); 
+  pinMode(pinoSentido2, OUTPUT);
+  pinMode(LED, OUTPUT);
   pinMode(pinoPWM, OUTPUT);
   Serial.begin(9600);
   Serial.setTimeout(5);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    valor_pwm = Serial.parseFloat();
+  if (!Serial.available() > 0) {
+    // valor_pwm = Serial.parseFloat();
     digitalWrite(pinoSentido1, LOW);   // controle do sentido de rotação do motor
     digitalWrite(pinoSentido2, HIGH);  // controle do sentido de rotação do motor
+    digitalWrite(LED, HIGH);
     analogWrite(pinoPWM, valor_pwm);
 
     valorAD = analogRead(pinoAD);          // recebe o pino analogico A3
