bc00999fec3dc779e70d31eedaab96833b9946c7
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-04-19 22:19:52 -0300

    repositorio atualizado

--- Identificacao_de_Sistemas/firmwares_microcontroladores/PlatformIo/arduino_uno/src/main.cpp
+++ Identificacao_de_Sistemas/firmwares_microcontroladores/PlatformIo/arduino_uno/src/main.cpp
@@ -1,16 +1,10 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
 #include "Arduino.h"
 
 int pinoAD = A0;
 int pinoPWM = 10;
 
 int valorAD = 0;
-int valor_pwm = 130;
+int valor_pwm = 120;
 
 int pinoSentido1 = 8;
 int pinoSentido2 = 9;
@@ -28,14 +22,14 @@ void setup() {
 void loop() {
   if (!Serial.available() > 0) {
     // valor_pwm = Serial.parseFloat();
-    digitalWrite(pinoSentido1, LOW);   // controle do sentido de rotação do motor
-    digitalWrite(pinoSentido2, HIGH);  // controle do sentido de rotação do motor
+    digitalWrite(pinoSentido1, LOW);
+    digitalWrite(pinoSentido2, HIGH);
     digitalWrite(LED, HIGH);
     analogWrite(pinoPWM, valor_pwm);
 
-    valorAD = analogRead(pinoAD);          // recebe o pino analogico A3
-    Serial.println(valorAD * 5. / 1023.);  // escreve o valor da porta analogica na porta serial e converte
+    valorAD = analogRead(pinoAD);
+    Serial.println(valorAD * 5. / 1023.);
     delay(20);
-  }  // Fim do IF
+  }
 
-}  // Fim do loop
+}
