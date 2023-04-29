7922ea0aaabf68ccee8b2a401a9be19d48da7007
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-04-29 18:27:10 -0300

    repositorio atualizado

--- Identificacao_de_Sistemas/firmwares_microcontroladores/PlatformIo/Esp32_ttgo/src/main.cpp
+++ Identificacao_de_Sistemas/firmwares_microcontroladores/PlatformIo/Esp32_ttgo/src/main.cpp
@@ -1,103 +1,129 @@
-/*
-************************       ****************************
-
+/*************************  | |  ****************************
       * Universidade Federal do Pará
       * Campus Universitário de Tucuruí
       * Faculdade de Engenharia Elétrica
-      * Trabalho de Conclusão de Curso
+      * Trabalho de Conclusão de Curso - Aeropêndulo
 
       * Título : Firmware Potótipo Aeropêndulo
       * Professor Orientador: Raphael Teixeira
       * Autor  : Oséias Farias
 
-      * Arquivo: main.cpp
-
-      * Data: 2023
-           
-           ***************       **************
-*/
+      * Arquivo: main.cpp    |   Data: 2023
+           ***************   |   ***************/
 
 #include "Arduino.h"
 
-const int pinoAD_POT = 2;         // Valor do potenciômetro
-const int pinoAD_CONTROL = 13;    // Sinal de Controle
+const int pinoAD_POT = 2;         // Valor do potenciômetro.
+const int pinoAD_CONTROL = 13;    // Sinal de Controle.
 
-// Define a direção de rotação do motor
+// Define a direção de rotação do motor.
 const int pinoSentido1 = 32;
 const int pinoSentido2 = 33;
 
 // Configurações do Sinal PWM
-const int pinoPWM = 25;           // pino para sinal PWM
-const int freq = 500;             // Frequência do sinal PWM
-const int pwmChannel = 0;         // Canal para o sinal PWM (0-15)
-const int resolution = 8;         // Resolução do sinal PWM
-int dutyCycle = 120;              // Ciclo de trabalho
-float ref1 = 90.0;                // Setpoint
-float erro = 7.0;                 // Erro do sistema
+const int pinoPWM = 25;           // pino para sinal PWM.
+const int freq_pwm = 500;         // Frequência do sinal PWM.
+const int canal_pwm = 0;          // Canal para o sinal PWM (0-15).
+const int resolucao = 8;          // Resolução do sinal PWM.
+int ciclo_trabalho = 120;         // Ciclo de trabalho.
+float ref1 = 90.0;                // Setpoint.
+float erro = 7.0;                 // Erro do sistema.
 
 // Variáveis usadas no código
-int valorAD_POT = 0;              // Valor de tensão (potenciômetro) lido pela conversor ADC
+int valorAD_POT = 0;              // Valor de tensão (potenciômetro) lido pela conversor ADC.
 float tensao_pot = 0.0;           // Tensão no potenciômetro.
 
-float theta = 0.0;                // Ângulo theta
-float tensao_control = 0.0;       // Sinal de controle em tensão
-// Variável para salvar o sinal de controle, obtido pelo conversor AD.
+float theta = 0.0;                // Ângulo theta.
+float tensao_control = 0.0;       // Sinal de controle em tensão.
+/* Variável para salvar o sinal de controle, obtido pelo conversor AD. */
 int valorAD_CONTROL = 0;
 
+/* Parâmetros do sinal de referência */
+float ampl = 0.4;
+float freq_ref;
+float offset;
+
+/* Potótipos de Funções */
+void enviar_dados_serial();
+void ler_dados_serial();
 
 void setup() {
   Serial.begin(115200);
+  Serial.setTimeout(5);
   while (!Serial)
     delay(10);
 
-  // Define os pinos como saída.
+  /* Define os pinos como saída. */
   pinMode(pinoSentido1, OUTPUT);
   pinMode(pinoSentido2, OUTPUT);
   pinMode(pinoPWM, OUTPUT);
 
-  // Define o sentido de Giro do Motor
+  /* Define o sentido de Giro do Motor */
   digitalWrite(pinoSentido1, HIGH);
   digitalWrite(pinoSentido2, LOW);
 
-  // Configura o sinal PWM usando para controle de velocidade do motor CC
-  ledcSetup(pwmChannel, freq, resolution);
-  ledcAttachPin(pinoPWM, pwmChannel);
+  /* Configura o sinal PWM usando para controle de velocidade do motor CC. */
+  ledcSetup(canal_pwm, freq_pwm, resolucao);
+  ledcAttachPin(pinoPWM, canal_pwm);
 }
 
-
 void loop() {
-  if (!Serial.available()) {
-    // valor_pwm = Serial.parseFloat();
-    ledcWrite(pwmChannel, dutyCycle);
 
-    // Sinal de Referência
+  enviar_dados_serial();
+  delay(10);
+
+  ler_dados_serial();
+  delay(10);
+}
+
+void enviar_dados_serial(){
+    // valor_pwm = Serial.parseFloat();
+    ledcWrite(canal_pwm, ciclo_trabalho);
+
+    /* Sinal de Referência. */
     Serial.print(ref1);
     Serial.print(",");
 
-    // Sinal de tensão no potenciômetro.
+    /* Sinal de tensão no potenciômetro. */
     valorAD_POT = analogRead(pinoAD_POT);
-    //Serial.print(valorAD_POT);
     theta = map(valorAD_POT, 528., 3235., 0., 180.);
     Serial.print(theta);
     Serial.print(",");
 
-    // Sinal de Erro
+    /* Sinal de Erro. */
     Serial.print(erro);
     Serial.print(",");
 
-    // Sinal de tensão aplicado ao motor cc.
+    /* Sinal de tensão aplicado ao motor cc. */
     valorAD_CONTROL = analogRead(pinoAD_CONTROL);
     tensao_control = valorAD_CONTROL * 3.3 / 4095.;
     Serial.print(tensao_control);
     Serial.print(",");
 
-    // Estruturas reservas de envio de dados
+    /* Estruturas reservas de envio de dados. */
     Serial.print(10.0);
-    Serial.print(",");
-    Serial.print(10.0);
+    Serial.print(",ampl");
+    Serial.print(ampl);
+    Serial.print(",offset");
+    Serial.print(offset);
+    Serial.print(",freq");
+    Serial.print(freq_ref);
     Serial.println(" ");
+}
 
-    delay(20);
-  }
+void ler_dados_serial(){
+      float rlen = Serial.parseFloat();
 
+    /* Dados do Amplitude. */
+    if (1000.0 < rlen && rlen < 2001.0){
+      ampl = rlen;
+
+    /* Dados do Frequência. */
+    } else if(2001.0 < rlen && rlen < 3001.0){
+      freq_ref = rlen;
+
+    /* Dados do offset. */
+    }else if(3001.0 < rlen && rlen < 4001.0){
+      offset = rlen;
+    }
 }
