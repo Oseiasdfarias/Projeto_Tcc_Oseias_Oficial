f0c96bfd3a321f49ba328fb46487a798e9c0e2bb
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-05-03 01:56:51 -0300

    repositorio atualizado

--- interface_aeropendulo/README.md
+++ interface_aeropendulo/README.md
@@ -22,10 +22,3 @@ Inteface para plotagem dos gráficos dos estados do sistema.
 </center>
 </div>
 
-<center>
-<div class="figure" >
-  <img src="interface.png"
-       width="900">  
-  <p>Figura 1 - Design Interface Aeropêndulo.</p>
-</div>
-</center>
--- interface_aeropendulo/rungui.py
+++ interface_aeropendulo/rungui.py
@@ -1,3 +1,16 @@
+"""**********************  | |  *************************
+      * Universidade Federal do Pará
+      * Campus Universitário de Tucuruí
+      * Faculdade de Engenharia Elétrica
+      * Trabalho de Conclusão de Curso - Aeropêndulo
+
+      * Título : Interface Gráfica do Aeropêndulo
+      * Professor Orientador: Raphael Teixeira
+      * Autor  : Oséias Farias
+
+      * Arquivo: main.cpp    |   Data: 2023
+           ***************   |   ***************"""
+
 from interface import InterfaceAeropendulo
 
 
