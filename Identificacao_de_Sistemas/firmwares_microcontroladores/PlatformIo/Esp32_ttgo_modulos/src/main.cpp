d6365bf94604b5b48f18a2968cea9b6e9e950eba
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-05-31 14:44:32 -0300

    repositorio atualizado

--- Identificacao_de_Sistemas/firmwares_microcontroladores/PlatformIo/Esp32_ttgo_modulos/src/main.cpp
+++ Identificacao_de_Sistemas/firmwares_microcontroladores/PlatformIo/Esp32_ttgo_modulos/src/main.cpp
@@ -12,6 +12,7 @@
            ***************   |   ************** */
 
 #include "Arduino.h"
+#include "ler_escrever_serial.h"
 
 const int pinAD_POT = 2;            // Valor do potenciômetro.
 
@@ -28,12 +29,7 @@ int ciclo_trabalho = 140;           // Ciclo de trabalho.
 float ref1 = 90.0;                  // Setpoint.
 float erro = 7.0;                   // Erro do sistema.
 
-// Variáveis usadas no código
-int valorAD_POT = 0;                // Valor de tensão (potenciômetro) lido pela conversor ADC.
-float tensao_pot = 0.0;             // Tensão no potenciômetro.
-
 float theta = 0.0;                  // Ângulo theta.
-float tensao_control = 0.0;         // Sinal de controle em tensão.
 /* Variável para salvar o sinal de controle, obtido pelo conversor AD. */
 int valorAD_CONTROL = 0;
 
@@ -42,9 +38,6 @@ float ampl = 0.4;
 float freq_ref;
 float offset;
 
-/* Potótipos de Funções */
-void enviar_dados_serial();
-void ler_dados_serial(float *erro, float *freq_ref, float *offset);
 
 void setup() {
   Serial.begin(115200);
@@ -68,58 +61,10 @@ void setup() {
 }
 
 void loop() {
-
-  enviar_dados_serial();
+  enviar_dados_serial(canal_pwm, pinAD_POT, &ciclo_trabalho,
+                      &ref1, &theta, &erro, &freq_ref, &ampl);
   delay(20);
-
   if (Serial.available() > 0){
     ler_dados_serial(&erro, &freq_ref, &offset);
   }
-  //delay(10);
-}
-
-void enviar_dados_serial(){
-    ledcWrite(canal_pwm, ciclo_trabalho);
-    
-    /* Sinal de Referência. */
-    Serial.print(ref1, 3);
-    Serial.print(",");
-
-    /* Sinal de tensão no potenciômetro. */
-    valorAD_POT = analogRead(pinAD_POT);
-    theta = map(valorAD_POT, 528., 3235., 0., 180.);
-    Serial.print(theta, 3);
-    Serial.print(",");
-
-    /* Sinal de Erro. */
-    Serial.print(erro, 3);
-    Serial.print(",");
-
-    /* Sinal de tensão aplicado ao motor cc. */
-    tensao_control = (ciclo_trabalho * 3.3 / 255.0);
-    Serial.print(tensao_control, 3);
-    Serial.print(",");
-
-    /* Estruturas reservas de envio de dados. */
-    Serial.print(freq_ref, 3);
-    Serial.print(",");
-    Serial.println(ampl, 3);
-  }
-
-void ler_dados_serial(float *erro, float *freq_ref, float *offset){
-  /* Leitura dos dados da porta serial. */
-    float rlen = Serial.parseFloat();
-
-    /* Dados do Amplitude. */
-    if (1000.0 < rlen && rlen < 2001.0){
-      *erro = (((rlen * 15.0) / 1000.0) - 15.0);
-
-    /* Dados do Frequência. */
-    } else if(2001.0 < rlen && rlen < 3001.0){
-      *freq_ref = (((rlen * 15.0) / 1000.0) - 30.0);
-
-    /* Dados do offset. */
-    }else if(3001.0 < rlen && rlen < 4001.0){
-      *offset = (((rlen * 15.0) / 1000.0) - 45.0);
-    }
 }
