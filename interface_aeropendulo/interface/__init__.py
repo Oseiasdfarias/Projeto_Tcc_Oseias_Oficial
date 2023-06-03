9b947240dd3405bab9c57ed465137c445df368f6
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-06-02 21:25:13 -0300

    repositorio atualizado

--- interface_aeropendulo/interface/__init__.py
+++ interface_aeropendulo/interface/__init__.py
@@ -17,4 +17,4 @@ from .coleta_dados import ColetaDados                  # noqa: F401
 from .graficos_sinais import GraficosSinais            # noqa: F401
 from .interface_grafica import InterfaceAeropendulo    # noqa: F401
 
-__version__ = "0.1.0"
+__version__ = "0.5.0"
