4d50fa79dea6cd796584e3275fd0428b38bd15ce
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-09-25 18:31:48 -0300

    atualização do repositório com novos arquivos

--- interface_aeropendulo/simulador_aeropendulo/__init__.py
+++ interface_aeropendulo/simulador_aeropendulo/__init__.py
@@ -16,9 +16,6 @@
 from .graficos_aeropendulo import Graficos                  # noqa: F401
 from .animacao_aeropendulo import AnimacaoAeropendulo       # noqa: F401
 from .simulador import Simulador                            # noqa: F401
-from .interface_interativa import Interface                 # noqa: F401
-from .modelo_mat_aeropendulo import ModeloMatAeropendulo    # noqa: F401
-from .implementacao_controlador import ControladorDiscreto  # noqa: F401
 
 
 __version__ = "0.5.0"
